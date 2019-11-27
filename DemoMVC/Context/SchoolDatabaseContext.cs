8fec32e0aa7f189e1382b2b1f9b7191e0035e85c
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-11-27 21:36:07 +0100

    Add project files.

---
+++ DemoMVC/Context/SchoolDatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using DemoMVC.Models;
+
+namespace DemoMVC.Context
+{
+    public class SchoolDatabaseContext : DbContext
+    {
+        public SchoolDatabaseContext(DbContextOptions<SchoolDatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ DemoMVC/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        [Display(Name = "Enroll Date")]
+        public DateTime EnrollDate { get; set; }
+    }
+}
